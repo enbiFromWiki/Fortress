949b0abb84698036b999f8ef45463060315f046e
enbi <lawfulbaguette@gmail.com>
2026-06-16 20:41:42 +0100

    Added username to oauth cookies

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -1,14 +1,14 @@
 export function Home() {
   async function fetchDetails() {
-    const res = await fetch("/call", {
+    const res = await fetch("/call2", {
       credentials: "include",
     });
     const data = await res.json();
     let html;
     if (!res.ok) {
-      html = `error: ${JSON.stringify(data)}`;
+      html = `error: ${JSON.stringify(data, null, 4)}`;
     } else {
-      html = JSON.stringify(data);
+      html = JSON.stringify(data, null, 4);
     }
 
     document.querySelector("#data-res")!.innerHTML = html;
@@ -18,7 +18,7 @@ export function Home() {
     <>
       <h1>Homepage</h1>
       <div onClick={fetchDetails}>Call API</div>
-      <div id="data-res"></div>
+      <pre id="data-res" style={{ fontSize: "1.2em" }}></pre>
     </>
   );
 }
